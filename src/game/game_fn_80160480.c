bc63bf81a04d2573b775ca3ae9ff4855d106d3dd
psdwizzard <k.loebenberg@gmail.com>
2026-09-12 19:02:24 -0400

    progress: 41.51% of code matched
    Source-commit: 0ecaba31aa7656d57b01a4cbbe852691be697a9a

--- src/game/game_fn_80160480.c
+++ src/game/game_fn_80160480.c
@@ -3,18 +3,20 @@ typedef struct Entry {
     char pad4[12];
 } Entry;
 
-extern unsigned char* lbl_8023A878[];
+extern unsigned char* volatile lbl_8023A878[];
 extern char lbl_8024F554[];
 extern char lbl_8024F578[];
 extern void fn_80163BB4(void*, const char*, ...);
 
 void fn_80160480(void* object, Entry* entry)
 {
-    int previous_type = entry[-2].type;
     int current_type = entry[-1].type;
+    int previous_type = entry[-2].type;
+    unsigned char* current_descriptor = lbl_8023A878[current_type];
+    unsigned char* previous_descriptor = lbl_8023A878[previous_type];
 
-    if ((signed char)lbl_8023A878[current_type][2] ==
-        (signed char)lbl_8023A878[previous_type][2])
+    if ((signed char)previous_descriptor[2] ==
+        (signed char)current_descriptor[2])
         fn_80163BB4(object, lbl_8024F554);
     else
         fn_80163BB4(object, lbl_8024F578);
