1dc365f9373f4e5c5676574b7c92493074873a5d
psdwizzard <k.loebenberg@gmail.com>
2026-09-12 19:06:35 -0400

    progress: 41.51% of code matched
    Source-commit: c14874cd4b4ca0ab294b07f753710b19b921d861

--- src/game/game_fn_801E8B6C.c
+++ src/game/game_fn_801E8B6C.c
@@ -36,10 +36,10 @@ void fn_801E8B6C(RangeControl* control, short amount)
         }
     } else if (control->value > control->maximum) {
         if (control->flags & 1) {
+            int minimum = control->minimum;
             int wrapped = control->value - control->maximum;
-            wrapped += control->minimum;
-            wrapped--;
-            control->value = wrapped;
+            minimum += wrapped;
+            control->value = minimum - 1;
         } else {
             control->value = control->maximum;
         }
