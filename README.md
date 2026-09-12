bc8a18a48702886e1d068b2c1defe4af4856948b
psdwizzard <k.loebenberg@gmail.com>
2026-09-11 21:37:53 -0400

    progress: 41.19% of code matched
    Source-commit: 6b745bc292f6283f0d5ec2d5dc05d97319d73f71

--- README.md
+++ README.md
@@ -54,9 +54,9 @@ Verified DOL SHA-1: `ea24b6af954876ce072562ff39cdb4c81d32be1f`.
 
 | Metric | Matched | Total | Percent |
 | --- | ---: | ---: | ---: |
-| Code bytes | 947436 | 2300692 | 41.18% |
-| Functions | 5668 | 8214 | 69.00% |
-| Objects (TUs) | 5903 | 6939 | 85.07% |
+| Code bytes | 947628 | 2300692 | 41.19% |
+| Functions | 5669 | 8214 | 69.02% |
+| Objects (TUs) | 5904 | 6939 | 85.08% |
 <!-- progress:end -->
 
 <!-- contributors:start -->
--- src/game/game_fn_801F0BF0.c
+++ src/game/game_fn_801F0BF0.c
@@ -10,33 +10,28 @@ void fn_801F0BF0(u8* context, void* arg, s32 upper_half)
 {
     u8* item;
     s32 i;
-    void** cursor;
     void** lists = (void**)(context + 0x138);
 
     fn_801ED468(0x1B);
     if (upper_half != 0) {
         i = 0x1000;
-        cursor = lists + 0x1000;
         do {
-            item = *cursor;
+            item = lists[i];
             while (item != 0) {
                 fn_801F08B8(item, context, arg);
                 item = *(u8**)(item + 0x10);
             }
             i++;
-            cursor++;
         } while (i < 0x2000);
     } else {
         i = 0;
-        cursor = lists;
         do {
-            item = *cursor;
+            item = lists[i];
             while (item != 0) {
                 fn_801F08B8(item, context, arg);
                 item = *(u8**)(item + 0x10);
             }
             i++;
-            cursor++;
         } while (i < 0x1000);
     }
 }
