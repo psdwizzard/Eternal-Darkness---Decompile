caf13b76cdb79da0899b161fb867034e1d632bf4
psdwizzard <k.loebenberg@gmail.com>
2026-09-13 05:20:38 -0400

    progress: 41.83% of code matched
    Source-commit: d9ef654598045195154d5ebcd9f1b54dfb539f66

--- src/game/game_fn_801ADC50.c
+++ src/game/game_fn_801ADC50.c
@@ -8,7 +8,14 @@ typedef struct Entry {
     unsigned char remainder[28];
 } Entry;
 
-extern Entry lbl_8060B430;
+typedef struct State {
+    Entry entries[3];
+    unsigned int initialized;
+    unsigned int interrupts;
+    unsigned int interrupt_depth;
+} State;
+
+extern State lbl_8060B430;
 extern int lbl_8064D308;
 extern void fn_801AE530(void);
 extern unsigned int fn_801BA2A4(unsigned int, unsigned int, unsigned int,
@@ -21,20 +28,16 @@ void fn_801ADC50(void)
 {
     Entry* entry;
     unsigned int index;
-    void (*callback)(void);
-    unsigned int callback_type;
     unsigned int zero;
 
     if (lbl_8064D308 == 0) {
         index = 0;
-        entry = &lbl_8060B430;
-        callback = fn_801AE530;
-        callback_type = 0x30001;
+        entry = &lbl_8060B430.entries[index];
         zero = 0;
         do {
             entry->handle = fn_801BA2A4(0xFF, entry->first, entry->second,
                                         0x7D00, 0, 0x40, 0, 0, zero, zero,
-                                        callback_type, callback, entry, zero);
+                                        0x30001, fn_801AE530, entry, zero);
             entry++;
             index++;
         } while (index < 3);
