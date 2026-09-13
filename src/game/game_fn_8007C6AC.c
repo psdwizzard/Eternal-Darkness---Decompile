fb779c815fee0933b337e6fe3be756fda2384760
psdwizzard <k.loebenberg@gmail.com>
2026-09-12 20:02:52 -0400

    progress: 41.55% of code matched
    Source-commit: 45f4fc468bbb8eb81f42993c612b9e5e1fcf5260

--- src/game/game_fn_8007C6AC.c
+++ src/game/game_fn_8007C6AC.c
@@ -24,36 +24,38 @@ extern void fn_8007D0F0(void);
 s32 fn_8007C6AC(void *object)
 {
     s32 object_id;
-    s32 shifted_id;
     void *resource;
-    u32 kind_or_created;
+    void *manager;
     void *current;
+    u32 kind;
+    void *created;
+    s32 shifted_id;
     s32 result;
 
     object_id = fn_80201B54(object);
-    current = fn_8004918C();
+    manager = fn_8004918C();
     result = -1;
-    current = fn_801A7778(current);
-    kind_or_created = 10;
+    current = fn_801A7778(manager);
+    kind = 10;
     resource = fn_80201BC8(object);
     if (fn_8011EB04(resource) == 0x51 && current != 0 && fn_801578AC(current) == 0x10)
-        kind_or_created = 0xA0;
-    kind_or_created = (u32)fn_801294DC(resource, kind_or_created, 0, 6);
-    if (kind_or_created != 0) {
+        kind = 0xA0;
+    created = fn_801294DC(resource, kind, 0, 6);
+    if (created != 0) {
         fn_8004918C();
         resource = fn_80201BC8(object);
         shifted_id = object_id << 8;
-        fn_80128C28((void *)kind_or_created, fn_80204810, shifted_id | 6);
+        fn_80128C28(created, fn_80204810, shifted_id | 6);
         fn_80201D2C(object, 0x4D);
         fn_80201D14(object, 1);
         if (fn_80129334(resource, 1, &result, -1) == -1) {
             fn_80157A28(current, fn_80157948(current));
             fn_80157C98(current, 0x20, 0);
         } else {
-            fn_801287C4((void *)kind_or_created, fn_8007D0F0, current, result);
+            fn_801287C4(created, fn_8007D0F0, current, result);
         }
-        fn_80128C28((void *)kind_or_created, fn_80204810, shifted_id | 6);
-        fn_80128C44((void *)kind_or_created, fn_80204810, shifted_id | 7);
+        fn_80128C28(created, fn_80204810, shifted_id | 6);
+        fn_80128C44(created, fn_80204810, shifted_id | 7);
     }
     return 1;
 }
