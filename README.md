d709d9debf589e15cf2eef285b5e67f360144878
psdwizzard <k.loebenberg@gmail.com>
2026-09-13 06:05:28 -0400

    progress: 41.86% of code matched
    Source-commit: 5062e6624a00ee058e62435df3f27ae782a95708

--- README.md
+++ README.md
@@ -56,7 +56,7 @@ Verified DOL SHA-1: `ea24b6af954876ce072562ff39cdb4c81d32be1f`.
 | --- | ---: | ---: | ---: |
 | Code bytes | 963072 | 2300692 | 41.86% |
 | Functions | 5788 | 8214 | 70.47% |
-| Objects (TUs) | 6023 | 7051 | 85.42% |
+| Objects (TUs) | 6023 | 7052 | 85.41% |
 <!-- progress:end -->
 
 <!-- contributors:start -->
---
+++ src/game/game_fn_80205914.c
@@ -0,0 +1,126 @@
+typedef unsigned int u32;
+
+extern u32 lbl_806515D0;
+extern u32 lbl_806515D4;
+extern u32 lbl_806515D8;
+extern float lbl_806515DC;
+extern float lbl_806515E0;
+extern float lbl_806515E4;
+extern u32 lbl_80651F70;
+
+extern int fn_80200C10(void *data);
+extern int fn_80200C38(void *data);
+extern void *fn_80155DB4(void *object);
+extern void *fn_80201BC8(void *object);
+extern void *fn_80201B54(void *object);
+extern u32 fn_8011FAEC(void *object);
+extern int fn_8011EB1C(void *object);
+extern void *fn_80126880(void *object);
+extern void fn_8020104C(int type, void *a, void *b, int value, float amount);
+extern void fn_8020123C(int type, void *a, void *b, int value);
+extern void fn_80201D2C(void *object, int value);
+extern void fn_80201D14(void *object, int value);
+extern void fn_801E8328(int value, void *object);
+extern void fn_801568C0(void *object, void (*callback)(void));
+extern void fn_8012C198(void *object);
+extern void fn_8011FABC(void *object, int value, int other);
+extern void fn_8012C774(void *object, u32 *a, u32 *b, u32 *c, int value);
+extern void fn_8011FA8C(void *object, int value, int flags);
+extern void fn_80205DB8(void);
+
+int fn_80205914(void *object, int event, void *data)
+{
+    int data_type = fn_80200C10(data);
+    void *handle = fn_80155DB4(object);
+    void *target = fn_80201BC8(object);
+    void *effect = fn_80201B54(object);
+
+    if (event == 0) {
+        if (data_type == 1) {
+            if (fn_8011FAEC(target) & 0x2000) {
+                fn_8020104C(0x11, effect, effect, 0, lbl_806515DC);
+                fn_80201D2C(object, 0xB);
+                fn_80201D14(object, 1);
+            } else {
+                fn_80201D2C(object, 1);
+                fn_80201D14(object, 1);
+            }
+            return 1;
+        }
+        if (data_type == 0x39) {
+            fn_801E8328(2, object);
+            fn_80201D2C(object, 0);
+            fn_80201D14(object, 1);
+            return 1;
+        }
+        if (data_type == 0x3D) {
+            fn_80126880(fn_80201BC8(object));
+            fn_8020123C(0x39, effect, effect, 0);
+            return 1;
+        }
+        if (data_type == 0x59) {
+            fn_80126880(fn_80201BC8(object));
+            fn_8020123C(0x39, effect, effect, 0);
+            return 1;
+        }
+        if (data_type == 0x1C) {
+            if (fn_80200C38(data) != 0 && fn_8011EB1C(target) == 6) {
+                fn_80126880(target);
+                fn_8020123C(0x39, effect, effect, 0);
+            }
+            return 1;
+        }
+        if (data_type == 0xE5) {
+            if (fn_8011EB1C(target) == 6) {
+                fn_80126880(target);
+                fn_8020123C(0x39, effect, effect, 0);
+            }
+            return 1;
+        }
+    } else if (event == 1) {
+        if (data_type == 0x47) {
+            fn_80201D2C(object, 0x23);
+            fn_80201D14(object, 1);
+            return 1;
+        }
+        if (data_type == 0x11) {
+            fn_80201D2C(object, 0xB);
+            fn_80201D14(object, 1);
+            return 1;
+        }
+    } else if (event == 0x23) {
+        if (data_type == 1) {
+            fn_801568C0(handle, fn_80205DB8);
+            fn_8012C198(target);
+            fn_8011FABC(target, 8, 0);
+            return 1;
+        }
+        if (data_type == 0x2E) {
+            fn_8020104C(0x11, effect, effect, 0, lbl_806515E0);
+            fn_80201D2C(object, 0xB);
+            fn_80201D14(object, 1);
+            return 1;
+        }
+    } else if (event == 0xB) {
+        if (data_type == 0x11) {
+            u32 output[3];
+            volatile u32 a = lbl_806515D0;
+            volatile u32 b = lbl_806515D4;
+            volatile u32 alternate = lbl_806515D8;
+            volatile u32 c = lbl_80651F70;
+
+            if (!(fn_8011FAEC(target) & 0x2000)) {
+                b = alternate;
+            }
+            output[0] = c;
+            output[1] = b;
+            output[2] = a;
+            fn_8012C774(target, &output[2], &output[1], &output[0], 4);
+            fn_8011FA8C(target, 0, 0x2000);
+            fn_8020104C(0x39, effect, effect, 0, lbl_806515E4);
+            return 1;
+        }
+        return 0;
+    }
+    return 0;
+}
