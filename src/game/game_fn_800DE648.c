9ff2751e21cd86e89566bb87ff8148f04165c10d
psdwizzard <k.loebenberg@gmail.com>
2026-09-14 14:50:54 -0400

    progress: 43.96% of code matched
    Source-commit: 10a477e609e0075b6617bebd1caf42191eddcd60

--- src/game/game_fn_800DE648.c
+++ src/game/game_fn_800DE648.c
@@ -13,16 +13,15 @@ extern void fn_8012B7A0(void *);
 void fn_800DE648(void *first, Vec3 *target, void *object)
 {
     void *info = fn_80201BC8(object);
-    Vec3 returned = fn_80201E78(object);
+    Vec3 copy;
     {
+        Vec3 returned = fn_80201E78(object);
         Vec3 position = returned;
 
         fn_8012B7D0(first, &position);
         fn_8012B7A0(first);
-        {
-            Vec3 copy = *target;
-            fn_8012B7D0(info, &copy);
-            fn_8012B7A0(info);
-        }
     }
+    copy = *target;
+    fn_8012B7D0(info, &copy);
+    fn_8012B7A0(info);
 }
