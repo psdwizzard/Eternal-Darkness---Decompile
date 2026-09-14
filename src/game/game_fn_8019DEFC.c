89c64724c226b690dc7e755cce421eab59b26380
psdwizzard <k.loebenberg@gmail.com>
2026-09-14 15:15:19 -0400

    progress: 43.96% of code matched
    Source-commit: 0c877a2331899c16f0f681507aacc938bb8119d5

--- src/game/game_fn_8019DEFC.c
+++ src/game/game_fn_8019DEFC.c
@@ -3,15 +3,20 @@ typedef signed short s16;
 
 void fn_8019DEFC(u8* object)
 {
-    u8* state = object + 0x8C;
-    s16 delta = 0;
-    s16* timer = (s16*)state;
-    int i = 0;
+    s16* timer;
+    u8* state;
+    int i;
+    s16 delta;
+
+    state = object + 0x8C;
+    delta = 0;
+    timer = (s16*)state;
+    i = 0;
 
     while (i < object[1]) {
         state[i + 0x4F] = 0;
         if (timer[6] <= 0) {
-            timer[6] = 0;
+            timer[6] = delta;
             delta -= state[0x71];
         } else {
             state[i + 0x4F] = 1;
