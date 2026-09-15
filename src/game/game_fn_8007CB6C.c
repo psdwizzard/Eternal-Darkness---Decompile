9925bedfb269ed8a53ca284f908bbbeb5c0b761a
psdwizzard <k.loebenberg@gmail.com>
2026-09-15 09:30:43 -0400

    progress: 44.41% of code matched
    Source-commit: 089f2fd720c76645e3ba12c03482d57ce61b1931

--- src/game/game_fn_8007CB6C.c
+++ src/game/game_fn_8007CB6C.c
@@ -23,29 +23,28 @@ extern void fn_8007D0F0(void);
 /* NonMatching: honest reconstruction of the paired target creation path. */
 s32 fn_8007CB6C(void *object)
 {
-    s32 object_id;
     void *resource;
+    s32 object_id;
     void *created;
     s32 first;
     void *second;
-    s32 shifted_id;
     s32 lookup;
-    s32 result = -1;
+    s32 result;
 
     object_id = fn_80201B54(object);
     fn_8004918C();
+    result = -1;
     resource = fn_80201BC8(object);
     created = fn_801294DC(resource, 10, 0, 6);
     if (created != 0) {
         fn_8004918C();
         first = fn_80157FE0(fn_80158598(object_id, 0), 1, 0);
-        shifted_id = object_id << 8;
-        fn_80128C28(created, fn_80204810, shifted_id | 6);
-        fn_80128C44(created, fn_80204810, shifted_id | 7);
+        fn_80128C28(created, fn_80204810, (object_id << 8) | 6);
+        fn_80128C44(created, fn_80204810, (object_id << 8) | 7);
         lookup = fn_80129364(resource, 1, 14, &result, -1);
         if (first == -1) {
             fn_80157FE0(fn_80158598(object_id, 0), 2, 0);
-            fn_80201814(0);
+            fn_80201814();
             second = fn_80201C24();
             if (lookup == -1) {
                 fn_80157A28(second, fn_80157948(second));
@@ -60,7 +59,7 @@ s32 fn_8007CB6C(void *object)
                 fn_80157A28(second, fn_80157948(second));
                 fn_80157C98(second, 0x20, 0);
                 fn_80157FE0(fn_80158598(object_id, 0), 2, 0);
-                fn_80201814(0);
+                fn_80201814();
                 second = fn_80201C24();
                 fn_80157A28(second, fn_80157948(second));
                 fn_80157C98(second, 0x20, 0);
