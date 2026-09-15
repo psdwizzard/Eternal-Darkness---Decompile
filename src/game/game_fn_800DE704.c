821672908727e04987021ce1a2fde351d9957b15
psdwizzard <k.loebenberg@gmail.com>
2026-09-15 17:57:08 -0400

    progress: 44.75% of code matched
    Source-commit: 42e67d14de6b7204eee17e2817da09b934beb552

--- src/game/game_fn_800DE704.c
+++ src/game/game_fn_800DE704.c
@@ -11,6 +11,9 @@ typedef struct SpawnInfo {
     u32 field_20;
     u32 field_24;
     u32 field_28;
+    u32 field_2C;
+    u32 field_30;
+    u32 field_34;
 } SpawnInfo;
 
 extern u32 lbl_8064D18C;
@@ -40,13 +43,18 @@ extern void* fn_80201C24();
 extern int fn_80157BC4(void);
 extern int fn_80157BF4(void *);
 extern void *fn_801E6CA0(void *, int, int, int, int);
-extern void fn_80027948(void *, int, void *, void *, int, int, int, int);
+extern void fn_80027948(void *, int, void *, void *, int, int, int);
 extern void fn_8002A590(void);
 extern void fn_8002A508(void);
 extern void fn_8002AC60(void);
 extern void fn_8002AA18(void);
 extern void fn_8002A4C8(void);
 
+/* NonMatching: behavior-complete object/action/event setup. The generated
+ * function is four bytes short because MWCC coalesces the fn_80201B54 result
+ * directly into its final register; retail retains an extra mr through r0.
+ * The remaining instruction differences are callee-saved register allocation.
+ */
 void fn_800DE704(void)
 {
     void *context = fn_80204A8C();
@@ -58,7 +66,6 @@ void fn_800DE704(void)
     void *object;
     void *info;
     void *handle;
-    void (*callback)(void) = fn_8002A590;
 
     fn_80201814(source);
     parent = fn_80201814(owner);
@@ -71,13 +78,14 @@ void fn_800DE704(void)
     spawn.position = position;
     object = fn_80034708(&spawn);
     info = fn_80201BC8(object);
-    parent = ((void *)fn_80201B54(object));
+    owner = ((void *)fn_80201B54(object));
     fn_80204CE4(object, context);
     fn_80201D54(object, lbl_8064D18C);
     fn_802015A4(object);
     handle = fn_80156DA0(3, &position);
-    if (fn_8011FCB0(info) != 0) callback = fn_8002A508;
-    fn_801568C8(handle, callback, fn_8002AC60, fn_8002AA18);
+    fn_801568C8(handle,
+                fn_8011FCB0(info) != 0 ? fn_8002A508 : fn_8002A590,
+                fn_8002AC60, fn_8002AA18);
     fn_80156904(handle, 0);
     fn_801568FC(handle, fn_8002AA18);
     fn_8015690C(handle, fn_8002A4C8);
@@ -86,5 +94,5 @@ void fn_800DE704(void)
     object = fn_80201C24(object);
     info = (void *)fn_80157BC4();
     object = fn_801E6CA0(lbl_8064C504, fn_80157BF4(object), (int)info, 0, 1);
-    fn_80027948(object, 0, source, parent, 0, 0, 0, 0);
+    fn_80027948(object, 0, source, owner, 0, 0, 0);
 }
