7695878039811357c90c30f7d41b79c866f3f62f
psdwizzard <k.loebenberg@gmail.com>
2026-09-15 16:52:38 -0400

    progress: 44.69% of code matched
    Source-commit: a3f0212b4afba321a7ef66f79891919f140d46d3

--- src/game/game_fn_8018C8FC.c
+++ src/game/game_fn_8018C8FC.c
@@ -6,16 +6,17 @@ extern u16 lbl_80607120[];
 extern unsigned int fn_800FBFB0(void);
 extern void* memcpy(void*, const void*, unsigned int);
 
-void fn_8018C8FC(u8* dest, const u8* color, u8 repeats, int run_length,
+void fn_8018C8FC(u8* dest, const u8* color, int repeats, int run_length,
                  u16 offset, u8 bits)
 {
     int mask = (1 << bits) - 1;
     int midpoint = 1 << (bits - 1);
+    u8 repeat_count = repeats;
     int i;
     int j;
     u8* cursor = dest;
 
-    for (i = 0; i < repeats; i++) {
+    for (i = 0; i < repeat_count; i++) {
         s8 delta = midpoint - (fn_800FBFB0() & mask);
         for (j = 0; j < run_length; j++) {
             cursor[0] = color[0] + delta;
