2dd67185366fe14f0ef71a0b44d9c16ee4ffd92d
psdwizzard <k.loebenberg@gmail.com>
2026-09-14 20:28:35 -0400

    progress: 44.13% of code matched
    Source-commit: 48a8b4e7ad55cb76f12ce49ab28b8e5468cd2151

--- src/game/game_fn_8007C90C.c
+++ src/game/game_fn_8007C90C.c
@@ -11,8 +11,8 @@ extern void *fn_801A7778(void *);
 extern void fn_80128C28();
 extern void fn_80128C44(void *, void *, s32);
 extern u16 fn_80157994(void *);
-extern u16 fn_80157948(void *);
-extern s32 fn_802053B0(void *, void *);
+extern s32 fn_80157948(void *);
+extern unsigned int fn_802053B0(void *, void *);
 extern void* fn_80201C24();
 extern void fn_80201D2C(void *, int);
 extern void fn_80201D14(void *, int);
@@ -26,32 +26,31 @@ extern void fn_8007CFB0(void);
 /* NonMatching: honest reconstruction of the target-sensitive creation path. */
 s32 fn_8007C90C(void *object)
 {
-    s32 object_id;
     void *resource;
-    void *target;
-    void *current;
-    void *created;
-    s32 shifted_id;
+    register void *created;
+    register void *target;
+    register void *current;
+    register s32 object_id;
+    register s32 target_index;
     s32 result = -1;
 
     object_id = fn_80201B54(object);
     fn_8004918C();
     resource = fn_80201BC8(object);
-    created = fn_801294DC(resource, 10, 0, 6);
-    if (created != 0) {
+    if ((created = fn_801294DC(resource, 10, 0, 6)) != 0) {
         current = fn_8004918C();
         fn_80201B44();
-        target = fn_801A7778(fn_80201814());
-        shifted_id = object_id << 8;
-        fn_80128C44(created, fn_80204810, shifted_id | 7);
-        object_id = (u16)fn_80157994(target) + 1;
-        if ((u16)fn_80157948(target) == object_id) {
-            fn_80128C28(created, fn_80204810, shifted_id | 6);
+        fn_80201814();
+        target = fn_801A7778(current);
+        fn_80128C44(created, fn_80204810, (object_id << 8) | 7);
+        target_index = (u16)fn_80157994(target) + 1;
+        if (target_index == (u16)fn_80157948(target)) {
+            fn_80128C28(created, fn_80204810, (object_id << 8) | 6);
         } else if (fn_802053B0(object, target) != 0) {
             if ((u16)fn_80157994(fn_80201C24()) != 1)
-                fn_80128C28(created, fn_80204810, shifted_id | 0xB3);
+                fn_80128C28(created, fn_80204810, (object_id << 8) | 0xB3);
             else
-                fn_80128C28(created, fn_80204810, shifted_id | 6);
+                fn_80128C28(created, fn_80204810, (object_id << 8) | 6);
         }
         resource = fn_80201BC8(object);
         fn_80201D2C(object, 0x48);
@@ -59,7 +58,7 @@ s32 fn_8007C90C(void *object)
         if (fn_80129334(resource, 1, &result, -1) == -1) {
             fn_80157A28(target, fn_80157948(target));
             fn_80157C98(target, 0x20, 0);
-            fn_80128C28(created, fn_80204810, shifted_id | 6);
+            fn_80128C28(created, fn_80204810, (object_id << 8) | 6);
         } else {
             fn_801287C4(created, fn_8007CFB0, current, result);
         }
