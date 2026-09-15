9bdb35e9334fe594f7d471c23e768d7b1d020bdd
psdwizzard <k.loebenberg@gmail.com>
2026-09-15 15:24:25 -0400

    progress: 44.67% of code matched
    Source-commit: 59e518a0e9e514f66d18885d4197df327e226886

--- src/game/game_fn_8013CBE4.c
+++ src/game/game_fn_8013CBE4.c
@@ -6,8 +6,8 @@ extern float fn_80211B44(const Vec3*, const Vec3*);
 extern float lbl_8065031C;
 extern float lbl_80650320;
 
-void fn_8013CBE4(const Vec3* origin, const Vec3* endpoint,
-                 const Vec3* point, Vec3* output, int clamp)
+float fn_8013CBE4(const Vec3* origin, const Vec3* endpoint,
+                  const Vec3* point, Vec3* output, int clamp)
 {
     Vec3 segment;
     Vec3 delta;
@@ -19,10 +19,17 @@ void fn_8013CBE4(const Vec3* origin, const Vec3* endpoint,
     fn_80211A6C(point, origin, &delta);
     amount = fn_80211B44(&delta, &segment) / length;
     if (clamp != 0) {
-        amount = amount > lbl_8065031C ? amount : lbl_8065031C;
-        amount = amount < lbl_80650320 ? amount : lbl_80650320;
+        float lower = lbl_8065031C;
+        float upper = lbl_80650320;
+        float maximum = amount > lower ? amount : lower;
+        if (upper < maximum) {
+            amount = upper;
+        } else {
+            amount = amount > lower ? amount : lower;
+        }
     }
     output->x = origin->x + segment.x * amount;
     output->y = origin->y + segment.y * amount;
     output->z = origin->z + segment.z * amount;
+    return amount;
 }
