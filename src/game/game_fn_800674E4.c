979aeaea40bee70fcab6bc9493abb67727fed5f5
psdwizzard <k.loebenberg@gmail.com>
2026-09-15 08:51:45 -0400

    progress: 44.38% of code matched
    Source-commit: ad1982cb190cc412fddf968df64de586ead293d5

--- src/game/game_fn_800674E4.c
+++ src/game/game_fn_800674E4.c
@@ -25,31 +25,43 @@ extern void fn_80201DD8();
 void fn_800674E4(s32 context, s32 event)
 {
     s32 actor = fn_80036D38(context);
-    s32 owner = fn_80201B54(context);
-    s32 owner_object = (s32)fn_80201B94(context);
-    s32 event_id = fn_80200C20(event);
+    s32 owner_object;
+    s32 owner;
+    u32 flags;
+    s32 event_id;
     s32 state;
-    s32 no_target = fn_80200C38(event) == -1;
+    s32 mode;
+    s32 has_target;
     s32 effect;
 
+    owner = fn_80201B54(context);
+    owner_object = (s32)fn_80201B94(context);
+    event_id = fn_80200C20(event);
+    has_target = fn_80200C38(event) != -1;
     state = (s32)fn_80201B8C(context);
-    if (*(u8 *)(state + 0x9F) >= 3 && *(u8 *)(state + 0x9F) < 5) {
-        effect = fn_801A717C();
-        fn_801A7470(effect, 0x10);
-        fn_801A74A0(effect, owner);
-        fn_801A74A8(effect, owner);
-        fn_8020123C(0x35, owner, owner, effect);
-        fn_801A7228(effect);
-        fn_800359A0(context, fn_80201814(event_id));
+    mode = *(u8 *)(state + 0x9F);
+    switch (mode) {
+        case 3:
+        case 4:
+            effect = fn_801A717C();
+            fn_801A7470(effect, 0x10);
+            fn_801A74A0(effect, owner);
+            fn_801A74A8(effect, owner);
+            fn_8020123C(0x35, owner, owner, effect);
+            fn_801A7228(effect);
+            fn_800359A0(context, fn_80201814(event_id));
+            break;
     }
-    if (no_target) {
-        state = fn_800AD2B4();
+    if (has_target) {
+        effect = fn_800AD2B4();
         if (lbl_8064D18C == 0x29 && fn_8015C9F0() == 0)
             fn_8016B400(0x47E, owner, 0);
-        *(u32 *)actor |= 0x00400000;
+        flags = *(u32 *)actor;
+        flags |= 0x00400000;
+        *(u32 *)actor = flags;
         *(s32 *)(actor + 0xC0) = event_id;
         fn_80201DD8(owner_object, 0);
-        if (state != 0 && event_id != state)
-            fn_8020123C(0x90, owner, state, 0);
+        if (effect != 0 && event_id != effect)
+            fn_8020123C(0x90, owner, effect, 0);
     }
 }
