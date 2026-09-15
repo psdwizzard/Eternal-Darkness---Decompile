4132068652c8c2e01f1cdadafa3651395a01083c
psdwizzard <k.loebenberg@gmail.com>
2026-09-15 06:58:34 -0400

    progress: 44.32% of code matched
    Source-commit: b0294ecbbe0ec445357aec4e2251117624dc9a51

--- src/game/game_fn_801FFE78.c
+++ src/game/game_fn_801FFE78.c
@@ -7,7 +7,7 @@ typedef struct Vec3 {
 
 extern float fn_80048C2C(float);
 extern float fn_80048C50(float);
-extern void fn_80179C10(Vec3*, float, float, const Vec3*, float, u8);
+extern void fn_80179C10(Vec3*, float, float, const Vec3*, float, int);
 extern int fn_80179EB8(const Vec3*);
 extern void fn_801ECD74(const void*);
 extern void fn_801ECF50(int);
@@ -22,6 +22,7 @@ extern void fn_80211AAC(const Vec3*, Vec3*);
 extern float fn_80211B44(const Vec3*, const Vec3*);
 extern void fn_80226AB4(int, int, int);
 extern void fn_80226D28(int);
+extern u8 lbl_802FC53C[];
 
 void fn_801FFE78(Vec3* first_center, Vec3* axis, Vec3* second_center,
                  u8 sides, u32* color, u8 alpha,
@@ -29,17 +30,19 @@ void fn_801FFE78(Vec3* first_center, Vec3* axis, Vec3* second_center,
                  float second_x, float second_y)
 {
     Vec3 direction;
-    Vec3 first_offset;
-    Vec3 second_offset;
     Vec3 first_point;
     Vec3 second_point;
+    Vec3 projection_offset;
+    Vec3 first_offset;
     Vec3 first_next;
+    Vec3 second_offset;
     Vec3 second_next;
     float angle;
     float first_depth;
     float second_depth;
     float sine;
     float cosine;
+    u32 render_color;
     int dominant;
     int i;
 
@@ -47,7 +50,7 @@ void fn_801FFE78(Vec3* first_center, Vec3* axis, Vec3* second_center,
         return;
     }
     fn_80211AAC(axis, &direction);
-    angle = 360.0f / sides;
+    angle = 0.01745329238474369f * (float)(360 / sides);
     first_depth = -fn_80211B44(first_center, &direction);
     second_depth = -fn_80211B44(second_center, &direction);
     dominant = fn_80179EB8(&direction);
@@ -65,19 +68,20 @@ void fn_801FFE78(Vec3* first_center, Vec3* axis, Vec3* second_center,
         if (second_x > 0.0f) second_x *= 0.5f;
     }
 
-    fn_80179C10(&first_offset, 0.0f, 0.0f, &direction, first_depth, dominant);
-    fn_80211A6C(first_center, &first_offset, &first_point);
-    fn_80179C10(&first_offset, 0.0f, 0.0f, &direction, second_depth, dominant);
-    fn_80211A6C(second_center, &first_offset, &second_point);
+    fn_80179C10(&projection_offset, 0.0f, 0.0f, &direction, first_depth, dominant);
+    fn_80211A6C(first_center, &projection_offset, &first_point);
+    fn_80179C10(&projection_offset, 0.0f, 0.0f, &direction, second_depth, dominant);
+    fn_80211A6C(second_center, &projection_offset, &second_point);
 
     fn_801ED468(0x1B);
     fn_80226D28(0);
     fn_80226D28(2);
     fn_801ED118();
-    fn_801EDA7C((void*)0x8030C53C, 0, 0x2BF, 0);
+    fn_801EDA7C(lbl_802FC53C, 0, 0x2BF, 0);
     fn_801ECF50(4);
     ((u8*)color)[3] = alpha;
-    fn_801ECD74(color);
+    render_color = *color;
+    fn_801ECD74(&render_color);
 
     sine = fn_80048C2C((sides - 1) * angle);
     cosine = fn_80048C50((sides - 1) * angle);
@@ -90,8 +94,8 @@ void fn_801FFE78(Vec3* first_center, Vec3* axis, Vec3* second_center,
     fn_80226AB4(0x80, 3, sides * 4);
 
     for (i = 0; (u8)i < sides; i++) {
-        sine = fn_80048C2C(i * angle);
-        cosine = fn_80048C50(i * angle);
+        sine = fn_80048C2C((u8)i * angle);
+        cosine = fn_80048C50((u8)i * angle);
         fn_80179C10(&first_offset, cosine * first_x, sine * first_y,
                     &direction, first_depth, dominant);
         fn_80211A48(&first_offset, &first_point, &first_offset);
