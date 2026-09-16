d0c0e669449a1c409ed96f4e7d50f8e7a00271bf
psdwizzard <k.loebenberg@gmail.com>
2026-09-16 19:44:47 -0400

    progress: 45.55% of code matched
    Source-commit: 3e58cc326cf3a0dfb58c0bd6dae422b6d839b683

--- src/game/game_fn_8017BBD0.c
+++ src/game/game_fn_8017BBD0.c
@@ -32,29 +32,29 @@ int fn_8017BBD0(u32 value, u8 kind, const char* text, u32 flags, u32 user_data)
 {
     u16 index = lbl_8064A7C8.write_index;
     u16 next = index + 1;
+    int difference = next - 6;
     MessageEntry* entry;
 
-    next = next < 6 ? next : 0;
-    if (next == lbl_8064A7C8.read_index) {
-        return 0;
+    next = difference < 0 ? next : 0;
+    if (next != lbl_8064A7C8.read_index) {
+        entry = &lbl_8064A7C8.entries[index];
+        entry->value = value;
+        entry->field_08 = -1;
+        entry->kind = kind;
+        if (text != 0) {
+            fn_800FD40C(entry->text, text);
+        }
+        entry->flags = flags;
+        entry->field_40 = 0;
+        entry->time = OSGetTime();
+        entry->user_data = user_data;
+        if (entry->flags & 1) {
+            entry->state = 3;
+        } else {
+            entry->state = 0;
+        }
+        lbl_8064A7C8.write_index = next;
+        return 1;
     }
-
-    entry = &lbl_8064A7C8.entries[index];
-    entry->value = value;
-    entry->field_08 = -1;
-    entry->kind = kind;
-    if (text != 0) {
-        fn_800FD40C(entry->text, text);
-    }
-    entry->flags = flags;
-    entry->field_40 = 0;
-    entry->time = OSGetTime();
-    entry->user_data = user_data;
-    if (entry->flags & 1) {
-        entry->state = 3;
-    } else {
-        entry->state = 0;
-    }
-    lbl_8064A7C8.write_index = next;
-    return 1;
+    return 0;
 }
