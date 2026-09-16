c0fdc7ec60da6c235de64e8f1d9678ced28d5ba5
psdwizzard <k.loebenberg@gmail.com>
2026-09-16 19:04:06 -0400

    progress: 45.55% of code matched
    Source-commit: c356768d1e5fc286839f4f50183687b7f72d8578

--- src/game/game_fn_801D9D14.c
+++ src/game/game_fn_801D9D14.c
@@ -19,10 +19,10 @@ extern void fn_801B05B0(u32, u32);
 extern void fn_801D0E78(Object*);
 extern u32 fn_801D3944(u32);
 extern u32 fn_801D3A34(u32, u32);
-extern u32 fn_801CEB2C(u32);
+extern u8 fn_801CEB2C(u32);
 extern void fn_8014E9B0(void*, void*, u32, u32, u32*, u32, u32, u32);
 extern void* fn_80201814(u32);
-extern u32 fn_80201B64(void*);
+extern int fn_80201B64(void*);
 extern void fn_801FE934(u32, u32);
 extern void fn_801D9A68(Object*);
 extern void fn_801858E0(void*);
@@ -35,11 +35,10 @@ extern void fn_801E8328(u32, void*);
 void fn_801D9D14(Object* self)
 {
     u32 owner;
-    u32 value;
     u8* effect;
-    u32 xyz[3];
+    u32 parameter;
 
-    if (*(u32*)(self->bytes + 8) != lbl_8064D18C ||
+    if (*(int*)(self->bytes + 8) != lbl_8064D18C ||
         (*(u8*)(self->bytes + 0xFF0) & 1) != 0) {
         fn_801FE22C(*(u32*)(self->bytes + 0x44));
         if (*(u16*)(self->bytes + 0xFF4) <= 0x62 &&
@@ -50,7 +49,7 @@ void fn_801D9D14(Object* self)
         if ((*(u8*)(self->bytes + 0xFF0) & 0x10) != 0)
             fn_801D1318(0);
         fn_801A9E40(-1);
-        if (*(u32*)(self->bytes + 0x10) != -1U)
+        if (*(int*)(self->bytes + 0x10) != -1)
             fn_801B05B0(*(u32*)(self->bytes + 0x10), 10);
         fn_801D0E78(self);
         return;
@@ -58,37 +57,43 @@ void fn_801D9D14(Object* self)
 
     owner = *(u32*)(self->bytes + 4);
     if ((*(u16*)(self->bytes + 0xFF4) & 0xF) == 0) {
-        xyz[0] = fn_801D3944(owner);
-        value = fn_801D3A34(owner, 0x35);
+        parameter = fn_801D3944(owner);
         fn_8014E9B0(self->bytes + 0x16C, self->bytes + 0x38,
-                    fn_801CEB2C(owner), value, xyz, 3, 4, 1);
+                    fn_801CEB2C(owner), fn_801D3A34(owner, 0x35),
+                    &parameter, 3, 4, 1);
     }
 
     switch (*(u16*)(self->bytes + 0xFF4)) {
     case 0x3C: {
         void* node = fn_80201814(*(u32*)(self->bytes + 0xC));
         if (node != 0 && fn_80201B64(node) != 8) {
+            u32 effectOwner;
             u32 init[2];
+            u32 xyz[3];
+            float zOffset;
             init[0] = lbl_80651EE0;
             *(u16*)((u8*)init + 4) = lbl_80651EE4;
             fn_801FE934(*(u32*)(self->bytes + 0x44), 0x3C);
             fn_801D9A68(self);
             effect = self->bytes + 0xBC;
             fn_801858E0(effect);
-            fn_801D38BC(fn_801D38E8(owner), effect + 0x78, effect + 4);
+            effectOwner = fn_801D38E8(owner);
             effect[1] = 0x80;
             *(u16*)(effect + 8) = 20;
             *(u16*)(effect + 6) = 45;
-            effect[3] = (u8)-10;
+            *(signed char*)(effect + 3) = -10;
+            fn_801D38BC(effectOwner, effect + 0x78, effect + 4);
             effect[0x14] = 30;
             *(u16*)(effect + 0x1C) = 0x80;
+            zOffset = lbl_80651160;
             effect[0x18] |= 2;
             effect[0x19] = 8;
             *(void (**)(void))(effect + 0x90) = fn_80185AE8;
             xyz[0] = *(u32*)(self->bytes + 0x38);
             xyz[1] = *(u32*)(self->bytes + 0x3C);
             xyz[2] = *(u32*)(self->bytes + 0x40);
-            *(float*)&xyz[2] += lbl_80651160;
+            zOffset += *(float*)&xyz[2];
+            *(float*)&xyz[2] = zOffset;
             *(u32*)(effect + 0x98) = xyz[0];
             *(u32*)(effect + 0x9C) = xyz[1];
             *(u32*)(effect + 0xA0) = xyz[2];
