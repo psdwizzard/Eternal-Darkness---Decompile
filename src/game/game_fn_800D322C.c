f21533cce728088dc52acb63bfe9eefd8ba82eeb
psdwizzard <k.loebenberg@gmail.com>
2026-09-16 00:38:56 -0400

    progress: 45.03% of code matched
    Source-commit: c21918a2f223bd44cf3b6ca9d3f9ddd09d631c06

--- src/game/game_fn_800D322C.c
+++ src/game/game_fn_800D322C.c
@@ -8,6 +8,18 @@ typedef struct Vec3 {
     u32 z;
 } Vec3;
 
+typedef struct Initializers {
+    Vec3 first;
+    Vec3 second;
+} Initializers;
+
+typedef struct CallLocals {
+    u32 extra;
+    u32 lower;
+    u32 upper;
+    u32 position_x;
+} CallLocals;
+
 typedef struct ActorState {
     char pad_000[0x197];
     s8 active;
@@ -32,38 +44,32 @@ extern void fn_800A4634(ActorState *, void *);
 void fn_800D322C(ActorState *state, void *object, int enabled, int value)
 {
     u16 flags = 0x80;
-    volatile Vec3 first;
-    volatile Vec3 second;
-    Vec3 position;
-    u32 upper;
-    u32 lower;
-    u32 a = lbl_8064F380;
-    u32 b = lbl_8064F384;
-    u32 c = lbl_8064F388;
-    u32 d = lbl_8064F38C;
-
-    first.x = d;
-    first.z = c;
-    second.x = b;
-    second.z = a;
+    Initializers initializers;
+    volatile CallLocals call;
+    initializers.second.z = lbl_8064F380;
+    initializers.second.x = lbl_8064F384;
+    initializers.first.z = lbl_8064F388;
+    initializers.first.x = lbl_8064F38C;
 
     if (enabled != 0) {
         flags |= 0x32;
-        first.y = d;
-        second.y = c;
+        initializers.first.y = initializers.first.x;
+        initializers.second.y = initializers.first.z;
     } else {
         flags |= 0x12;
-        first.y = a;
-        second.y = b;
+        initializers.first.y = initializers.second.z;
+        initializers.second.y = initializers.second.x;
     }
 
     if (state->handle != -1) {
-        fn_8012DBE8(object, 15, (u32 *)&first.y);
-        position.x = first.y;
-        lower = second.y;
-        upper = first.y;
+        fn_8012DBE8(object, 15, &initializers.first.y);
+        call.lower = initializers.first.y;
+        call.upper = initializers.second.y;
+        call.position_x = initializers.first.y;
+        call.extra = initializers.first.y;
         fn_800A1AF0(object, state->handle, enabled, value,
-                    &position, &lower, &upper, flags);
+                    (Vec3 *)&call.position_x, (u32 *)&call.upper,
+                    (u32 *)&call.lower, flags);
     }
     if (state->active != 0) {
         fn_800A3C84(object, state->handle, enabled, value);
