87d8a0d0acaff39f76b737d692285b07ffca8934
psdwizzard <k.loebenberg@gmail.com>
2026-09-16 17:38:31 -0400

    progress: 45.54% of code matched
    Source-commit: b8106a9864b8516c0a26ed3f7b6bf19020e73c6e

--- src/game/game_fn_8012FF34.c
+++ src/game/game_fn_8012FF34.c
@@ -1,7 +1,8 @@
 typedef unsigned char u8;
+typedef unsigned int u32;
 
 typedef struct Vec3 {
-    float x, y, z;
+    u32 x, y, z;
 } Vec3;
 
 typedef struct RuntimeState {
@@ -23,17 +24,29 @@ extern float lbl_806501DC;
 extern void fn_80125ECC(void *);
 extern int fn_8012FFE0(void*, int, int);
 
-void fn_8012FF34(Object* object, Vec3* position, int flags, int index)
+int fn_8012FF34(Object* object, Vec3* position, int flags, int index)
 {
+    register u32 x;
+    register u32 y;
+    register Vec3* destination;
     float one;
+    int result;
 
     fn_80125ECC(object);
-    if (fn_8012FFE0(object, flags, index)) {
+    result = fn_8012FFE0(object, flags, index);
+    if (result) {
+        destination = &object->runtime->position;
+        x = position->x;
+        y = position->y;
         one = lbl_806501DC;
-        object->runtime->position = *position;
+        destination->x = x;
+        destination->y = y;
+        destination->z = position->z;
         object->runtime->flags = flags;
         object->runtime->state = 2;
         object->runtime->index = index;
         object->runtime->scale = one;
     }
+
+    return result;
 }
