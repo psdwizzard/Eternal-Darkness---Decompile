eebba19259dcb5cb4cdd955ede45169b6c7caf0e
psdwizzard <k.loebenberg@gmail.com>
2026-09-17 10:07:15 -0400

    progress: 45.98% of code matched
    Source-commit: cd747a180d3dc7d66f3245b0594954eee4aba8b8

--- src/game/game_fn_801BE354.c
+++ src/game/game_fn_801BE354.c
@@ -12,42 +12,29 @@ typedef struct StreamState {
     u8 pad_000[0x114];
     u32 flags_114;
     u8 pad_118[0x58];
-    u32 base_170[6];
-    u32 step_188[6];
+    u32 base_170[2];
+    u32 delta_178[2];
+    u32 target_180[2];
+    u32 step_188[2];
 } StreamState;
-
-typedef struct ChannelState {
-    u8 pad_000[0x170];
-    u32 base_170;
-    u8 pad_174[4];
-    u32 delta_178;
-    u8 pad_17C[4];
-    u32 target_180;
-} ChannelState;
 
 extern void fn_801CC408(u32*);
 
 void fn_801BE354(StreamState* state, StreamCommand* command, u8 index)
 {
-    register s32 value;
-    u32 offset;
-    u32* step;
+    s32 value;
     s32 adjustment;
 
     value = command->value >> 16;
-    offset = index * 4;
-    step = &state->step_188[index];
-    *step = value;
-    fn_801CC408(step);
-    ((ChannelState*)((u8*)state + offset))->base_170 =
-        (command->value << 8) & 0x00FF0000;
+    state->step_188[index] = value;
+    fn_801CC408(&state->step_188[index]);
     adjustment = (s8)command->adjustment << 16;
-    ((ChannelState*)((u8*)state + offset))->target_180 =
-        ((ChannelState*)((u8*)state + offset))->base_170 + adjustment;
-    if (*step != 0) {
-        ((ChannelState*)((u8*)state + offset))->delta_178 = adjustment / value;
+    state->base_170[index] = (command->value << 8) & 0x00FF0000;
+    state->target_180[index] = state->base_170[index] + adjustment;
+    if (state->step_188[index] != 0) {
+        state->delta_178[index] = adjustment / value;
     } else {
-        ((ChannelState*)((u8*)state + offset))->delta_178 = adjustment;
+        state->delta_178[index] = adjustment;
     }
     state->flags_114 |= 0x2000;
 }
