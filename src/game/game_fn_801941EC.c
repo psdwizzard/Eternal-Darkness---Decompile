da355193b6dc85799c84389ca0ad93f46549b853
psdwizzard <k.loebenberg@gmail.com>
2026-09-17 15:14:09 -0400

    progress: 46.13% of code matched
    Source-commit: 36f8e39097f0796a11fad9e38c0630de6af5185d

--- src/game/game_fn_801941EC.c
+++ src/game/game_fn_801941EC.c
@@ -13,16 +13,25 @@ extern void fn_80193F3C(u8, int, Vec3s*, Vec3s*, float, float, s16, u8, u8);
 
 void fn_801941EC(u8* state, u8* input, Vec3s* output)
 {
-    Vec3s points[16];
+    Vec3s points[18];
     int i;
-    int limit;
+    int stride;
+    u8 count;
+    u8 interior;
+    u8 last;
+    u8 last2;
+    u8 last3;
     s16 offset;
     s16 step;
     float x_offset;
     float y_offset;
 
-    limit = state[0xB];
-    for (i = 1; i < limit - 1; i++) {
+    count = state[0xB];
+    interior = count - 4;
+    last = count - 1;
+    last2 = count - 2;
+    last3 = count - 3;
+    for (i = 1; i < last; i++) {
         fn_801870D0(input + 0xA, &points[i]);
         input += 0x38;
     }
@@ -30,12 +39,9 @@ void fn_801941EC(u8* state, u8* input, Vec3s* output)
     points[0].x = points[1].x - (points[2].x - points[1].x);
     points[0].y = points[1].y - (points[2].y - points[1].y);
     points[0].z = points[1].z - (points[2].z - points[1].z);
-    points[limit - 1].x = points[limit - 2].x +
-                          (points[limit - 2].x - points[limit - 3].x);
-    points[limit - 1].y = points[limit - 2].y +
-                          (points[limit - 2].y - points[limit - 3].y);
-    points[limit - 1].z = points[limit - 2].z +
-                          (points[limit - 2].z - points[limit - 3].z);
+    points[last].x = points[last2].x + (points[last2].x - points[last3].x);
+    points[last].y = points[last2].y + (points[last2].y - points[last3].y);
+    points[last].z = points[last2].z + (points[last2].z - points[last3].z);
 
     offset = *(s16*)(state + 0x22);
     step = *(s16*)(state + 0x24);
@@ -47,23 +53,25 @@ void fn_801941EC(u8* state, u8* input, Vec3s* output)
     output[1].z = points[1].z;
     output += 2;
 
-    for (i = 0; i < limit - 4; i++) {
+    for (i = 0; i < interior; i++) {
         fn_80187120(points[i + 1].x, points[i + 1].y,
                     points[i + 3].x, points[i + 3].y,
                     &x_offset, &y_offset);
-        fn_80193F3C(limit, i, &points[i + 2], output, x_offset, y_offset,
+        fn_80193F3C(state[0xB], i, &points[i + 2], output, x_offset, y_offset,
                     offset, state[4], state[5]);
         offset += step;
         output += 2;
     }
 
-    output[0].x = points[limit - 1].x;
-    output[0].y = points[limit - 1].y;
-    output[0].z = points[limit - 1].z;
-    output[1].x = points[limit - 2].x;
-    output[1].y = points[limit - 2].y;
-    output[1].z = points[limit - 2].z;
-    output += (limit * 2 + 2);
+    output[0].x = points[last].x;
+    output[0].y = points[last].y;
+    output[0].z = points[last].z;
+    output[1].x = points[last2].x;
+    output[1].y = points[last2].y;
+    output[1].z = points[last2].z;
+    stride = interior * 2 + 1;
+    output += 1;
+    output += stride;
     output[0].x = points[0].x;
     output[0].y = points[0].y;
     output[0].z = points[0].z;
@@ -76,14 +84,16 @@ void fn_801941EC(u8* state, u8* input, Vec3s* output)
     output[3].x = points[1].x;
     output[3].y = points[1].y;
     output[3].z = points[1].z;
-    output += (limit * 2 + 4);
-    output[0].x = points[limit - 2].x;
-    output[0].y = points[limit - 2].y;
-    output[0].z = points[limit - 2].z;
-    output[1].x = points[limit - 2].x;
-    output[1].y = points[limit - 2].y;
-    output[1].z = points[limit - 2].z;
-    output += (limit * 2 + 2);
+    output += 3;
+    output += stride;
+    output[0].x = points[last2].x;
+    output[0].y = points[last2].y;
+    output[0].z = points[last2].z;
+    output[1].x = points[last2].x;
+    output[1].y = points[last2].y;
+    output[1].z = points[last2].z;
+    output += 1;
+    output += stride;
     output[0].x = points[1].x;
     output[0].y = points[1].y;
     output[0].z = points[1].z;
