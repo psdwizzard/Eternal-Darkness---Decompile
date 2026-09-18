869a5a50fb4b3cbd926177fe1480c33833426391
psdwizzard <k.loebenberg@gmail.com>
2026-09-18 10:17:01 -0400

    progress: 46.22% of code matched
    Source-commit: 4644786f0fbd1601724898ba56a7ee92fcf46489

--- src/game/game_fn_80160480.c
+++ src/game/game_fn_80160480.c
@@ -12,11 +12,11 @@ void fn_80160480(void* object, Entry* entry)
 {
     int current_type = entry[-1].type;
     int previous_type = entry[-2].type;
-    unsigned char* current_descriptor = lbl_8023A878[current_type];
     unsigned char* previous_descriptor = lbl_8023A878[previous_type];
+    unsigned char* current_descriptor = lbl_8023A878[current_type];
 
-    if ((signed char)previous_descriptor[2] ==
-        (signed char)current_descriptor[2])
+    if ((signed char)current_descriptor[2] ==
+        (signed char)previous_descriptor[2])
         fn_80163BB4(object, lbl_8024F554);
     else
         fn_80163BB4(object, lbl_8024F578);
