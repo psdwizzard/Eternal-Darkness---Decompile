251db62d73624ddbd6c62a46760bf16f0feffd8d
psdwizzard <k.loebenberg@gmail.com>
2026-09-18 11:17:58 -0400

    progress: 46.22% of code matched
    Source-commit: 7a391be6e21ed56b5316c7c7a66551f4932ebd38

--- src/game/game_fn_800F623C.c
+++ src/game/game_fn_800F623C.c
@@ -1,12 +1,14 @@
 long long fn_800F623C(int hi, unsigned int lo, unsigned int shift)
 {
     unsigned int out_lo;
+    int shifted_hi;
     int under = 32 - shift;
     int over = shift - 32;
     out_lo = lo >> shift;
     out_lo |= hi << under;
+    shifted_hi = hi >> over;
     if (over > 0) {
-        out_lo |= hi >> over;
+        out_lo |= shifted_hi;
     }
     return ((unsigned long long)(hi >> shift) << 32) | out_lo;
 }
