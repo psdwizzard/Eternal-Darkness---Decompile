f2709452c20318f9493bb00ec29990e2224815e7
psdwizzard <k.loebenberg@gmail.com>
2026-09-18 14:22:37 -0400

    progress: 46.34% of code matched
    Source-commit: 13ace9c1906b0ed87ae6ba96eb87600105322abf

--- src/game/game_fn_8007D744.c
+++ src/game/game_fn_8007D744.c
@@ -12,21 +12,31 @@ extern void fn_801E6228(void *, const char *, ...);
 void fn_8007D744(int event)
 {
     int ticks;
-    int minutes;
-    int hours;
     u32 *flags;
 
-    if (lbl_8064C8EC != 0 && event == 5) {
+    if (lbl_8064C8EC == 0) {
+        return;
+    }
+    switch (event) {
+    case 0:
+        break;
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
         flags = fn_801E5D20(lbl_8064C8EC);
-        *flags |= 1;
         ticks = *(s16 *)(lbl_8031CBA0 + 0x14);
-        if (ticks < 0) {
-            ticks = 0;
-        }
-        hours = ticks / 3600;
-        minutes = ticks / 60;
-        fn_801E6228(lbl_8064C8EC, lbl_802448F8, hours,
-                    minutes - (minutes / 60) * 60,
-                    ticks - minutes * 60);
+        ticks = 0 > ticks ? 0 : ticks;
+        *flags |= 1;
+        fn_801E6228(lbl_8064C8EC, lbl_802448F8, ticks / 3600,
+                    (ticks / 60) % 60, ticks % 60);
+        break;
+    default:
+        break;
     }
 }
