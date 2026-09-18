4272e969f55c265f8777d9c046b9a7cbf6bbca63
psdwizzard <k.loebenberg@gmail.com>
2026-09-18 10:52:21 -0400

    progress: 46.22% of code matched
    Source-commit: 8ad5a3cc8c7b5f77f8434758a1428dcdc1078447

--- src/game/game_fn_800EEC44.c
+++ src/game/game_fn_800EEC44.c
@@ -5,34 +5,46 @@ extern void fn_800EEC0C(u8 *, int, int, int);
 
 void fn_800EEC44(u8 *input, u8 *output, int width, int height)
 {
+    u8 * volatile output_ptr = output;
+    int columns = width >> 1;
     int rows = height >> 1;
-    int y;
     u8 *middle = input + width * height;
-    u8 *lower = middle + (width >> 1) * rows;
+    u8 *lower = middle;
+    int y = 0;
 
-    for (y = 0; y < rows; y++) {
+    lower += columns * rows;
+
+    while (rows > 0) {
         u8 *top0 = input;
-        u8 *top1 = input + width;
-        int x;
-        u8 *dst = output + ((y >> 1) * width * 16) + ((y & 1) ? 16 : 0);
+        u8 *top1;
+        int remaining = columns;
+        int x = 0;
+        u8 *dst = output_ptr + ((y >> 1) * width * 16) + ((y & 1) ? 16 : 0);
 
-        for (x = 0; x < (width >> 1); x++) {
+        input += width;
+        top1 = input;
+        while (remaining > 0) {
             int cb = *lower++ - 128;
             int cr = *middle++ - 128;
             int blue = -52 * cb - 25 * cr + 0x2020;
-            int green = 102 * cb + 129 * cr + 0x2020;
-            int red = -25 * cb - 52 * cr + 0x2020;
+            int green = 102 * cb + 0x2020;
+            int red = 129 * cr + 0x2020;
             int lum = ((int)lbl_803281E0[*top0++] - 16) * 74;
             u8 *block = dst + ((x >> 1) * 64) + ((x & 1) ? 4 : 0);
+            u8 *block2 = block + 8;
 
             fn_800EEC0C(block, lum + green, lum + blue, lum + red);
             lum = ((int)lbl_803281E0[*top0++] - 16) * 74;
             fn_800EEC0C(block + 2, lum + green, lum + blue, lum + red);
             lum = ((int)lbl_803281E0[*top1++] - 16) * 74;
-            fn_800EEC0C(block + 8, lum + green, lum + blue, lum + red);
+            fn_800EEC0C(block2, lum + green, lum + blue, lum + red);
             lum = ((int)lbl_803281E0[*top1++] - 16) * 74;
-            fn_800EEC0C(block + 10, lum + green, lum + blue, lum + red);
+            fn_800EEC0C(block2 + 2, lum + green, lum + blue, lum + red);
+            x++;
+            remaining--;
         }
-        input += width * 2;
+        input += width;
+        y++;
+        rows--;
     }
 }
