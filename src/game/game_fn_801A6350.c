877b06ebb953a90822f2d719ff9ad624cccfea9c
psdwizzard <k.loebenberg@gmail.com>
2026-09-19 03:30:43 -0400

    progress: 47.01% of code matched
    Source-commit: b98812b0b7f45a0003ef8d93bd91e8fdf43a3c01

--- src/game/game_fn_801A6350.c
+++ src/game/game_fn_801A6350.c
@@ -2,35 +2,65 @@ int fn_801A6350(int state)
 {
     switch (state) {
     case -1:
-        return 0;
-    case 1:
-        return 2;
-    case 2:
-        return 3;
-    case 22:
-        return 1;
+        goto result0;
     case 0:
+        goto result4;
+    case 1:
+        goto result2;
+    case 2:
+        goto result3;
     case 3:
+        goto result4;
     case 4:
+        goto result4;
     case 5:
+        goto result4;
     case 6:
+        goto result4;
     case 7:
+        goto result4;
     case 8:
+        goto result4;
     case 9:
+        goto result4;
     case 10:
+        goto result4;
     case 11:
+        goto result4;
     case 12:
+        goto result4;
     case 13:
+        goto result4;
     case 14:
+        goto result4;
     case 15:
+        goto result4;
     case 16:
+        goto result4;
     case 17:
+        goto result4;
     case 18:
+        goto result4;
     case 19:
+        goto result4;
     case 20:
+        goto result4;
     case 21:
-        return 4;
+        goto result4;
+    case 22:
+        goto result1;
     default:
-        return 0;
+        goto result0;
     }
+
+result0:
+    return 0;
+result1:
+    return 1;
+result2:
+    return 2;
+result3:
+    return 3;
+result4:
+    return 4;
 }
