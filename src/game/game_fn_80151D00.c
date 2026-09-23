30b85db3d54c678d91ab5c27a66a1f6d55f0db84
psdwizzard <k.loebenberg@gmail.com>
2026-09-23 04:11:45 -0400

    progress: 48.91% of code matched
    Source-commit: 19259fa2fa6db61e7cd511d354e58f9a60f305a1

--- src/game/game_fn_80151D00.c
+++ src/game/game_fn_80151D00.c
@@ -45,9 +45,10 @@ extern void fn_80199EBC(void*);
 extern void fn_80199E18(void);
 extern void fn_8014F700(Vec3f*, s16, void*);
 extern void fn_80149E28(void*);
-void fn_80151D00(u8* instance)
+void fn_80151D00(u8* arg)
 {
-    u8* work = instance + 8;
+    u8* work = arg + 8;
+    u8* instance = arg;
     Vec3f position0;
     Vec3f position1;
     u8 key0[8];
@@ -87,10 +88,10 @@ void fn_80151D00(u8* instance)
         *(u32*)(descriptor + 0x38) = *(u32*)(work + 0x24);
         position1 = *(Vec3f*)(work + 0x2C);
         if (fn_80148008(&position1, key0, descriptor, fn_8019ADE4) != 0) {
-            void* object = fn_80156938();
-            fn_8017FF1C(object, 2);
-            fn_8017FE1C(object, fn_8018B058);
-            *(void**)(work + 0x254) = object;
+            arg = fn_80156938();
+            fn_8017FF1C(arg, 2);
+            fn_8017FE1C(arg, fn_8018B058);
+            *(void**)(work + 0x254) = arg;
         }
     } else if (FRAME >= END - 58 && FRAME <= END - 10 && *(void**)(work + 0x254) != 0) {
         fn_801809B8(*(void**)(work + 0x254), FRAME - (END - 56));
@@ -114,45 +115,43 @@ void fn_80151D00(u8* instance)
                      *(u32*)(work + 0x24), amount, optional,
                      *(u16*)(work + 0x0A));
     } else if (FRAME == END + 10 && *(s32*)work == 0) {
-        u8* command;
         Vec3s* object_position;
         *(u32*)key1 = lbl_80651C40;
         *(u16*)(key1 + 4) = lbl_80651C44;
-        command = work + 0x1A4;
-        fn_8019A3A4(command);
-        *(u16*)(command + 4) = *(s16*)(work + 0x10);
-        *(u16*)(command + 6) = *(u16*)(work + 0x14) - 20;
-        *(void**)(command + 0x14) = *(void**)(work + 0x254);
-        *(u32*)(command + 0x18) = *(u32*)(work + 0x24);
-        *(void (**)(void))(command + 0x90) = fn_8019A300;
+        arg = work + 0x1A4;
+        fn_8019A3A4(arg);
+        *(u16*)(arg + 4) = *(s16*)(work + 0x10);
+        *(u16*)(arg + 6) = *(u16*)(work + 0x14) - 20;
+        *(void**)(arg + 0x14) = *(void**)(work + 0x254);
+        *(u32*)(arg + 0x18) = *(u32*)(work + 0x24);
+        *(void (**)(void))(arg + 0x90) = fn_8019A300;
         object_position = fn_8017FDA8(*(void**)(work + 0x254), 0);
-        *(float*)(command + 0x98) = object_position->x;
-        *(float*)(command + 0x9C) = object_position->y;
-        *(float*)(command + 0xA0) = object_position->z;
-        memcpy(command + 0xA4, key1, 6);
-        *(u32*)(command + 0x94) = 0;
-        command[0xAA] = 2;
-        fn_801E8328(0x10, command);
+        *(float*)(arg + 0x98) = object_position->x;
+        *(float*)(arg + 0x9C) = object_position->y;
+        *(float*)(arg + 0xA0) = object_position->z;
+        memcpy(arg + 0xA4, key1, 6);
+        *(u32*)(arg + 0x94) = 0;
+        arg[0xAA] = 2;
+        fn_801E8328(0x10, arg);
     }
 
     if (*(s32*)(instance + 0x1324) == lbl_8064D18C) {
         if (FRAME == 0) {
-            u8* command;
             *(u32*)key2 = lbl_80651C48;
             *(u16*)(key2 + 4) = lbl_80651C4C;
-            command = work + 0x44;
-            fn_80199EBC(command);
-            *(u16*)(command + 6) = END - 10;
-            *(u16*)(command + 4) = *(s16*)(work + 0x0E);
-            *(u16*)(command + 0x20) = *(float*)(work + 0x2C);
-            *(u16*)(command + 0x22) = *(float*)(work + 0x30);
-            *(u16*)(command + 0x24) = *(float*)(work + 0x34);
-            *(u32*)(command + 0x2C) = *(u32*)(work + 0x24);
-            *(void (**)(void))(command + 0x90) = fn_80199E18;
-            memcpy(command + 0xA4, key2, 6);
-            *(u32*)(command + 0x94) = 0;
-            command[0xAA] = 4;
-            fn_801E8328(0x10, command);
+            arg = work + 0x44;
+            fn_80199EBC(arg);
+            *(u16*)(arg + 6) = END - 10;
+            *(u16*)(arg + 4) = *(s16*)(work + 0x0E);
+            *(u16*)(arg + 0x20) = *(float*)(work + 0x2C);
+            *(u16*)(arg + 0x22) = *(float*)(work + 0x30);
+            *(u16*)(arg + 0x24) = *(float*)(work + 0x34);
+            *(u32*)(arg + 0x2C) = *(u32*)(work + 0x24);
+            *(void (**)(void))(arg + 0x90) = fn_80199E18;
+            memcpy(arg + 0xA4, key2, 6);
+            *(u32*)(arg + 0x94) = 0;
+            arg[0xAA] = 4;
+            fn_801E8328(0x10, arg);
         }
         if (FRAME == *(u16*)(work + 0x1A) || FRAME == *(u16*)(work + 0x1A) + 10 ||
             FRAME == *(u16*)(work + 0x1A) + 20) {
